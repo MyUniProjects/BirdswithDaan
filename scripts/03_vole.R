d6b47051905aa443a33f8c697b585190b64f7979
Daan <d.kroeze.1@student.rug.nl>
2024-11-27 12:07:22 +0100

    Update

---
+++ scripts/03_vole.R
@@ -0,0 +1,13 @@
+# restore libraries
+rm(list = ls()) # clear environment
+
+# load libraries
+#
+
+env <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vToaxR52HV3eiUxu7jAWmT4DE_TRLJrLEOLHm0APmUN2dY1hCnnshFL7s117jiLqisTH-xAI16xS2Hr/pub?gid=1857363379&single=true&output=csv")
+
+vole <- 
+  
+#Assignment:
+  #Is food abundance affected by either Hellmann’s or NAO ?
+    #Vole abundance ~ Hellmann’s / NAO
