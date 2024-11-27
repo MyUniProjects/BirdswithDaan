3dd0a72a811e57c7c07d8043d0396f1ac8d7a5e9
Daan <d.kroeze.1@student.rug.nl>
2024-11-27 12:15:56 +0100

    Loaded data

--- scripts/03_vole.R
+++ scripts/03_vole.R
@@ -2,11 +2,13 @@
 rm(list = ls()) # clear environment
 
 # load libraries
-#
+library(tidyverse)
 
-env <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vToaxR52HV3eiUxu7jAWmT4DE_TRLJrLEOLHm0APmUN2dY1hCnnshFL7s117jiLqisTH-xAI16xS2Hr/pub?gid=1857363379&single=true&output=csv")
+env <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vToaxR52HV3eiUxu7jAWmT4DE_TRLJrLEOLHm0APmUN2dY1hCnnshFL7s117jiLqisTH-xAI16xS2Hr/pub?gid=1857363379&single=true&output=csv") |>
+  dplyr::select(year, Hellmann_winter, NAO_winter)
 
-vole <- 
+vole <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vToaxR52HV3eiUxu7jAWmT4DE_TRLJrLEOLHm0APmUN2dY1hCnnshFL7s117jiLqisTH-xAI16xS2Hr/pub?gid=1857363379&single=true&output=csv") |>
+  dplyr::select(year, voleMarch)
   
 #Assignment:
   #Is food abundance affected by either Hellmann’s or NAO ?
